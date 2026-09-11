720699e490fb29904598af4ae95ebab414d0f9c5
Mavine <mavinemalin@gmail.com>
2026-09-12 01:45:02 +0300

    update: TypeScript: images.ts | Images: default-profile-avatar-head.png [1 added, 1 removed]

--- constants/images.ts
+++ constants/images.ts
@@ -10,7 +10,7 @@ import noResult from "@/assets/images/no-result.png";
 import logoMark from "@/assets/images/baraka-logo-mark-v2.png";
 import logoWordmark from "@/assets/images/baraka-logo-wordmark.png";
 import authNairobi from "@/assets/images/auth-nairobi-residence.png";
-import defaultProfileAvatar from "@/assets/images/default-profile-avatar.jpg";
+import defaultProfileAvatar from "@/assets/images/default-profile-avatar-head.png";
 import tenantApartmentHome from "@/assets/images/tenant-apartment-home.jpg";
 
 export default {
